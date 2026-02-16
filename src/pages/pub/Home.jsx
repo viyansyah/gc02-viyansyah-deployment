d031a7e056b7066a21b957675671bf96a9fe7ac7
viyansyah <viyansyah@gmail.com>
2026-02-16 07:39:56 +0700

    feat:finish deploy

--- src/pages/pub/Home.jsx
+++ src/pages/pub/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Card from "../../components/card";
+import Card from "../../components/Card";
 import giftLoading from "../../assets/Loading.svg"
 import Logo from "../../assets/OKEI.png"
 import axios from 'axios'
